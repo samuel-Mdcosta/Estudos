34ed96478582d0c3601539622bb96ef1aeaeabc8
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2026-05-05 23:09:28 -0400

    [TESTE]- :key index

---
+++ problemas/vue/vforInstavel.vue
@@ -0,0 +1,19 @@
+//codigo errado
+<template>
+  <div v-for="(item, index) in lista" :key="index">
+    {{ item.nome }}
+  </div>
+</template>
+
+//codigo certo
+<template>
+  <div v-for="item in lista" :key="item.id">
+    {{ item.nome }}
+  </div>
+</template>
+
+#o v-for e itilizado para rendertizar uma lista de elementos baseado em array ou
+objeto #:key e necessario para performance e estabilidade da renderizacao
+#ajudando o vue a identificar quais itens doram adicionasos ou removidos
+#utilizar o index na jey faz com que o vue nao consiga identificar quais itens
+foram alterados, o que pode levar a bugs e problemas de performance
