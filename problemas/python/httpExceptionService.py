2e14a4ab96d7e4269cc862735bd4ae1f4077e6f0
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2026-05-05 20:26:58 -0400

    [TESTE] - httpexception no lugar errado

---
+++ problemas/python/httpExceptionService.py
@@ -0,0 +1,19 @@
+#funcao errada
+class DocumentoService:
+    def buscar(self, id: str):
+        doc = self.repo.find_by_id(id)
+        if not doc:
+            raise HTTPException(status_code=404, detail="Não encontrado")
+        return doc
+    
+#funcao correta
+class DocumentoService:
+    def buscar(self, id: str):
+        doc = self.repo.find_by_id(id)
+        if not doc:
+            raise DocumentosNaoEncontrados(f"Documentos {id} não encontrado")
+        return doc
+    
+#a camada de service ela deve ser especifica para regra de negocio
+#ela nao sabe sobre http, porem ela deveser capaz de lidar com erros, assim ela lanca esse erro para o arquivo handler
+#pois cada camada deve saber apenas sobre sua reponsabilidade
