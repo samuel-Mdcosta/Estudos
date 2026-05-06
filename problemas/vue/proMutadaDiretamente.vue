d05744ccc8028e6de9455e554d56c1384a8cf4b1
SAMUEL MARQUES DA COSTA <costadev19@gmail.com>
2026-05-05 21:43:26 -0400

    [TESTE] - prop sendo mudada diretamente

---
+++ problemas/vue/proMutadaDiretamente.vue
@@ -0,0 +1,20 @@
+//codigo errado
+<script setup>
+const props = defineProps(['usuario'])
+
+function salvar() {
+  props.usuario.nome = 'Novo Nome' // problema aqui
+}
+</script>
+
+//codigo certo
+<script setup>
+const props = defineProps(['usuario'])
+
+function salvar(){
+    emit('salvarUsuario', {props.usuario.nome = 'Novo Nome'})
+}
+
+#uma prop nao pode mudar diretamente pois o vue e reativo
+#para mudar o valor de uma prop deve avisar o pai que o valor dela foi alterado
+#para isso udamos o emit, que emite um evento para o pai
